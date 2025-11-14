a4563affba08cb8795a06980cea336c24490ec6f
Hermann <188955756+4483-s@users.noreply.github.com>
2025-11-14 13:29:44 +0800

    structure

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import ut from "../js/utils.js";
+function App() {
+  return (
+    <>
+      <div className="top"></div>
+      <div className="bottom"></div>
+    </>
+  );
+}
+
+export default App;
