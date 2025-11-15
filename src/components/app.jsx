281fdab505475eaec3f90c6e13904c689857a588
Hermann <188955756+4483-s@users.noreply.github.com>
2025-11-15 17:57:27 +0800

    state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,18 @@
 import ut from "../js/utils.js";
+import Init from "./init.jsx";
+import { useState } from "react";
 function App() {
+  const [initialised, setInitialised] = useState(false);
+  const [dif, setDif] = useState("easy");
+  function handleDif(dif) {
+    setDif(dif);
+  }
+  function handleInit(dif) {
+    setInitialised(true);
+    setDif(dif);
+  }
+
+  if (initialised) return <Init></Init>;
   return (
     <>
       <div className="top"></div>
